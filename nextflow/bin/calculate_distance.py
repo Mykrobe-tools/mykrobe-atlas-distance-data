466ec4fd3117dbc10e5fc120900b89adf89abcfb
Zhicheng Liu <Zhicheng-Liu@users.noreply.github.com>
2020-07-24 13:47:03 +0100

    Feature/distance calculation (#3)
    * Add sample names to the genotype calls output file
    
    * Revert "Add sample names to the genotype calls output file"
    
    This reverts commit 0fca84b1
    
    * Add sample names to the genotype calls output file
    
    * Calculate distance from two set of genotype calls
    
    * Update readme

--- nextflow/bin/calculate_distance.py
+++ nextflow/bin/calculate_distance.py
@@ -4,46 +4,56 @@ import argparse
 import numpy as np
 
 
-def _dist_two_samples(genotype_calls, i, j):
+def _dist_two_samples(genotype_calls_1, genotype_calls_2):
     return np.sum(
-            genotype_calls[i] * genotype_calls[j] * (genotype_calls[i] - genotype_calls[j]) != 0
+            genotype_calls_1 * genotype_calls_2 * (genotype_calls_1 - genotype_calls_2) != 0
         )
 
 
-def _calculate_distance_matrix(genotype_calls):
-    num_rows = len(genotype_calls)
-    num_cols = num_rows
-    distance_matrix = [[0] * num_cols for _ in range(num_rows)]
+def _calculate_1xn_distance(genotype_calls_1, genotype_calls_n):
+    n = len(genotype_calls_n)
+    distance_row = [0] * n
 
-    for row in range(num_rows):
-        for col in range(row+1, num_cols):
-            distance = _dist_two_samples(genotype_calls, row, col)
-            if distance != 0:
-                distance_matrix[row][col] = distance
-                distance_matrix[col][row] = distance
+    for index in range(n):
+        distance = _dist_two_samples(genotype_calls_1, genotype_calls_n[index])
+        if distance != 0:
+            distance_row[index] = distance
 
-    return distance_matrix
+    return distance_row
 
 
 def main():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('--genotype-calls', type=str)
-    arg_parser.add_argument('--out-distances', type=str)
+    arg_parser.add_argument('--genotype-calls1', type=str, required=True, help='genotype calls for samples in rows')
+    arg_parser.add_argument('--genotype-calls2', type=str, required=True, help='genotype calls for samples in columns')
+    arg_parser.add_argument('--out-distances', type=str, required=True, help='output file to store distance matrix')
     args = arg_parser.parse_args()
 
-    genotype_calls = []
-    with open(args.genotype_calls, "r") as f:
+    genotype_calls2 = []
+    samples_on_column = []
+    with open(args.genotype_calls2, "r") as f:
         for line in f:
+            line = line.rstrip('\n')
+            sample, genotype_calls = line.split('\t', maxsplit=1)
+            samples_on_column.append(sample)
             data = []
-            for c in line.rstrip('\n'):
+            for c in genotype_calls:
                 data.append(int(c))
-            genotype_calls.append(np.array(data, dtype=np.uint16))
-
-    distance_matrix = _calculate_distance_matrix(genotype_calls)
+            genotype_calls2.append(np.array(data, dtype=np.uint16))
 
     with open(args.out_distances, "w") as output:
-        for row in distance_matrix:
-            print(",".join([str(d) for d in row]), file=output)
+        print("", "\t", "\t".join(samples_on_column), file=output)
+        with open(args.genotype_calls1, "r") as f:
+            for line in f:
+                line = line.rstrip('\n')
+                sample, genotype_calls = line.split('\t', maxsplit=1)
+                data = []
+                for c in genotype_calls:
+                    data.append(int(c))
+                genotype_calls1 = np.array(data, dtype=np.uint16)
+
+                distance_row = _calculate_1xn_distance(genotype_calls1, genotype_calls2)
+                print(sample, "\t", "\t".join([str(d) for d in distance_row]), file=output)
 
     print("complete!")
 
